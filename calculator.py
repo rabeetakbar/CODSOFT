e64aee64ad82c0aeca5f146b6cb8884249169eb5
rabeetakbar <81568042+rabeetakbar@users.noreply.github.com>
2023-09-09 00:08:16 +0500

    Add files via upload

---
+++ calculator.py
@@ -0,0 +1,20 @@
+print("welcome to the python calculator app.")
+print("beware! only enter numbers no characters would be accepted.")
+while True:
+    try:
+        no1= float(input("enter 1st number: "))
+        no2= float(input("enter 2nd number: "))
+        break
+    except ValueError:
+        print("Invalid input. Please enter a number.")
+print("Now enter the arithmetic operation you want: \n +, -, *, /")
+sign=input(">>")
+if sign=="+":
+    result=no1+no2
+elif sign=="-":
+    result=no1-no2
+elif sign=="*":
+    result=no1*no2
+elif sign=="/":
+    result=no1/no2
+print("Your result is:",result)
